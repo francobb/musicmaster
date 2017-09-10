21e7fb47cde652b7b303ecee4df2529d2cd85cdb
francobb <jrizzle1790@gmail.com>
2017-09-09 22:06:44 -0400

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,26 @@
+import React, { Component } from 'react';
+
+class App extends Component {
+    render() {
+        return (
+          <div>
+              <div className="App-title">Music Master</div>
+              <div>
+                  <input/>
+                  <button>
+                      button
+                  </button>
+              </div>
+              <div className="Profile">
+                  <div>Artist Picture</div>
+                  <div>Artist Name</div>
+              </div>
+              <div className="Gallery">
+                  Gallery
+              </div>
+          </div>
+        );
+    };
+}
+
+export default App;
